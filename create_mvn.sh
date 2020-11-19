a1597d520b47a9998a261e06377ca9df2a2cce06
Passanisi Umberto <umberto_passanisi@carrefour.eu>
2020-11-19 07:02:16 +0100

    first commit

---
+++ create_mvn.sh
@@ -0,0 +1,7 @@
+mvn archetype:generate \
+  -DarchetypeArtifactId=google-cloud-dataflow-java-archetypes-starter \
+  -DarchetypeGroupId=com.google.cloud.dataflow \
+  -DgroupId=com.google.cloud.teleport.templates \
+  -DartifactId=templates2 \
+  -Dversion="[1.0.0,2.0.0]" \
+  -DinteractiveMode=false \
